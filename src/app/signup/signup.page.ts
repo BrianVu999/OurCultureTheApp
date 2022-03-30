687f216072ea193c4f2c7b73dd85ce92034cf25f
Junxiu Ma <55039335+Dearredhorse@users.noreply.github.com>
2022-03-29 21:50:46 -0400

    modified Login and signup page (#5)
    * modified Login and signup page
    
    * modified Login and signup page and add route link after signup successfully

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-signup',
@@ -14,29 +16,20 @@ export class SignupPage implements OnInit {
   conpassword = ""
   isSubmitted = false
 
-  constructor(public formBuilder:FormBuilder,public alertControl:AlertController) { }
+  validation_messages = {
+    'password': [
+
+      { type: 'validPassword', message: 'Password Not Match.' }
+    ]}
+
+  constructor(public formBuilder:FormBuilder,public alertControl:AlertController,private router: Router) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      password: ['', [Validators.required, Validators.pattern('^[a-z0-9._%$]+$')]],
-      conpassword: ['', [Validators.required, Validators.pattern('^[a-z0-9._%$]+$')]]
-    },
-    {
-      validator: this.checkMatchValidator('password', 'conpassword')
-    })
-  }
-
-  checkMatchValidator(field1: string, field2: string) {
-    return function (frm) {
-      let field1Value = frm.get(field1).value;
-      let field2Value = frm.get(field2).value;
-  
-      if (field1Value !== '' && field1Value !== field2Value) {
-        return { 'notMatch': `value ${field1Value} is not equal to ${field2}` }
-      }
-      return null;
-    }
+      password: ['', [Validators.required, Validators.pattern('^[a-z0-9._%$!]+$')]],
+      conpassword: ['', [Validators.required, Validators.pattern('^[a-z0-9._%$!]+$')]]
+    });
   }
   
   get errorControl() {
@@ -52,6 +45,19 @@ export class SignupPage implements OnInit {
     } else {
       console.log(this.signupForm.value)
     }
+
+    if(this.signupForm.get('conpassword').value==this.signupForm.get('password').value){
+      this.alertActionForSetUp()
+      this.router.navigate(['login']).then(nav=>{
+        //this.router.navigate([currentUrl]);
+        console.log(nav);
+        location.reload();
+      },err=>{ console.log(err);
+      });
+      
+    }else{
+      this.alertActionFaild() 
+    }
   }
 
   //show alert when missing the required data
@@ -63,6 +69,35 @@ export class SignupPage implements OnInit {
       buttons:[
        {text:'Ok',
         handler:()=>{console.log('Please provide all the required values!');}
+      }
+      ]
+    });
+    await alert.present();
+  }
+
+  //show alert
+  async alertActionForSetUp() {
+    const alert = await this.alertControl.create({
+      header:'Congratulation',
+      subHeader:'',
+      message:'You have signed up sccrssfully',
+      buttons:[
+       {text:'Ok',
+        handler:()=>{console.log('Sign up sccrssfully');}
+      }
+      ]
+    });
+    await alert.present();
+  }
+   //show alert
+   async alertActionFaild() {
+    const alert = await this.alertControl.create({
+      header:'Faild',
+      subHeader:'',
+      message:'Password not match, Please try again',
+      buttons:[
+       {text:'Ok',
+        handler:()=>{console.log('Sign up faild');}
       }
       ]
     });
