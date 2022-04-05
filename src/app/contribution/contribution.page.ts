2ae8e59a91e2756d46d174678fb889ebbd6c713b
Tanveer Kaur <59834257+Kaur6837@users.noreply.github.com>
2022-04-04 22:22:19 -0400

    Updated the error messages and validation for contribution page (#8)
    and after submitting the form, the user is taken to the main calendar page but the thing that I didn't do yet is the user must stay logged in after that.

--- src/app/contribution/contribution.page.ts
+++ src/app/contribution/contribution.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators  } from "@angular/forms";
+import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -9,45 +10,68 @@ import { AlertController } from '@ionic/angular';
 })
 export class ContributionPage implements OnInit {
    isSubmitted = false;
-  loginForm: FormGroup 
+  contributeForm: FormGroup 
+  alertMessage = ""
+  
 
-  constructor(public formBuilder:FormBuilder,public alertControl:AlertController) { }
+  constructor(public formBuilder:FormBuilder,public alertControl:AlertController ,private router: Router) { }
 
   
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
+    this.contributeForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.pattern('[A-Za-z0-9_]+$')]],
       date: ['', [Validators.required]],
       type: ['', [Validators.required]],
       religious: ['', [Validators.required]],
       same: ['', [Validators.required]],
-      reason: ['', [Validators.required]]
+      reason: ['', [Validators.required]],
+      ritual: ['', [Validators.required]]
     }) 
   }
 
-  submitForm() {
+submitForm() {
     //check the input
     this.isSubmitted = true;
-    if (!this.loginForm.valid) {
-      this.alert("Please provide all the required values")
-      return false;
-    } else {
-      console.log(this.loginForm.value)
-    }}
+    if(
+      this.contributeForm.get('name').value === "" ||
+      this.contributeForm.get('date').value === "" ||
+      this.contributeForm.get('type').value === "" ||
+      this.contributeForm.get('religious').value === "" ||
+      this.contributeForm.get('same').value === "" ||
+      this.contributeForm.get('reason').value === ""  ||
+      this.contributeForm.get('ritual').value === "" 
 
-    async alert(msg:string) {
-      const alert = await this.alertControl.create({
-        header:'Alert',
-        subHeader:'',
-        message:msg,
-        buttons:[
-         {text:'Ok',
-          handler:()=>{console.log(msg);}
+    ){
+      this.alertMessage = "Please enter all required fields!";
+    }
+    else if (!this.contributeForm.valid) {
+      this.alertMessage = "Please enter valid input!";
+    }else{
+      console.log("success")
+    this.contributeSucces("", "Your contribution is received")
+    }
+  }
+
+  async contributeSucces(header:string ,msg:string) {
+    const alert = await this.alertControl.create({
+      header:header,
+      subHeader:'',
+      message:msg,
+      buttons:[
+       {text:'Ok',
+        handler:()=>{
+          this.router.navigate(['home']).then(nav=>{
+            //this.router.navigate([currentUrl]);
+            console.log(nav)
+            location.reload();
+          },err=>{ console.log(err)
+          });
         }
-        ]
-      });
-      await alert.present();
-    } 
+      }
+      ]
+    });
+    await alert.present();
+  }
 
 
 }
