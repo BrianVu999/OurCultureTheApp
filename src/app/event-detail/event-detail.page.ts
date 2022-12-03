77c0aec60c316cd218cdb53525463694f646c17b
kaur6837 <kaur6837@sheridancollege.ca>
2022-12-03 18:11:07 -0500

    Multiple notifications set up done

--- src/app/event-detail/event-detail.page.ts
+++ src/app/event-detail/event-detail.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DatabaseService } from '../services/database.service';
 import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 import { FacebookAuthProvider } from '@angular/fire/auth';
 import { ELocalNotificationTriggerUnit, LocalNotifications } from '@ionic-native/local-notifications/ngx';
-import { Platform } from '@ionic/angular';
+import { AlertController, Platform } from '@ionic/angular';
+import {addDays} from "date-fns";
 
 @Component({
   selector: 'app-event-detail',
@@ -12,10 +13,13 @@ import { Platform } from '@ionic/angular';
 })
 export class EventDetailPage implements OnInit {
 
+  ids:number[]=[];
+  alertMessage = '';
+  @Input() selecteddays: any;
   selectedEvent:any;
   text :String;
   constructor(private dbService: DatabaseService,private socialSharing: SocialSharing, 
-    private localNotifications: LocalNotifications, private plt : Platform
+    private localNotifications: LocalNotifications, private plt : Platform, private alertControl : AlertController
     ) { 
     this.selectedEvent = dbService.selectedEvent.getValue;
     dbService.selectedEvent.subscribe(result => {
@@ -62,7 +66,7 @@ console.log("Click: ",res.title , msg, res.text);
                     "Date: "+this.messageDate+"\n"+
                     "Type: "+this.messageType+"\n"+
                     "Things to do: "+this.messageActivity+"\n"+
-                    "BY: #OURCULTUREAPP"
+                    "BY: #OURCULTURS"
   }
 
   shareViaTwitter() {
@@ -99,7 +103,7 @@ console.log("Click: ",res.title , msg, res.text);
     });
   }
 
-  notify(){
+ /*  notify(){
   this.localNotifications.schedule({
 id : 1,
 title: 'Reminder',
@@ -108,5 +112,83 @@ data : {mydata: 'My hidden message'},
 trigger: {in: 5, unit: ELocalNotificationTriggerUnit.SECOND},
 
   });
-  }
+  } */
+
+
+
+  setNotification(days : any ){
+
+
+   let id = this.ids.length;
+    this.ids.push(id);
+if(days.target.value == 4){
+
+  this.localNotifications.schedule({
+    id : id,
+    title: 'Reminder',
+    text: 'Event is coming(test 10 seconds)',
+    data : {mydata: 'My hidden message'},
+    trigger: {in: 10, unit: ELocalNotificationTriggerUnit.SECOND},
+    
+      });
+
+      
 }
+else if(days.target.value == 5){
+
+  this.localNotifications.schedule({
+    id : id,
+    title: 'Reminder',
+    text: 'Event is coming(test 15 seconds)',
+    data : {mydata: 'My hidden message'},
+    trigger: {in: 15, unit: ELocalNotificationTriggerUnit.SECOND},
+    
+      });
+}
+else{
+    this.selecteddays = days.target.value;
+    console.log(days);
+      var date = new Date(this.selectedEvent.date);
+      let beforeDays = addDays(date, -(this.selecteddays));
+      
+      console.log(beforeDays + ' ' + id);
+
+     
+  
+        this.localNotifications.schedule ({
+        
+        id : id,
+        title: 'Reminder',
+        text: this.selectedEvent.title + ' is coming in ' + this.selecteddays + ' days.',
+        trigger: {at: beforeDays},
+        data : {mydata: this.selectedEvent.title}
+      
+      });
+
+      
+}
+
+this.showAlert('', 'You will be notified before ' + days.target.value + ' days.');
+     
+
+    }
+
+    async showAlert(header: string, msg: string) {
+      const alert = await this.alertControl.create({
+        header: header,
+        subHeader: '',
+        message: msg,
+        buttons: [
+          {
+            text: 'Ok',
+            handler: () => {
+              
+            },
+          },
+        ],
+      });
+      await alert.present();
+    }
+
+    } 
+
